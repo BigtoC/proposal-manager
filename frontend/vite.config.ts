061361f385e98feb258350b2d54c048229429845
allthatjazzleo <leo.pang@mantra.finance>
2025-03-04 18:22:53 +0800

    fix: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/proposal-manager/',
   plugins: [react(), nodePolyfills()],
   resolve: {
     alias: {
