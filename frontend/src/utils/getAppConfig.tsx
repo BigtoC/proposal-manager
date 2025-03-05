8754d8a2177968c4809692c643db3b3d53527702
BigtoC <taitocdt@gmail.com>
2025-03-05 11:24:26 +0800

    chore(contract): Update contract config

--- frontend/src/utils/getAppConfig.tsx
+++ frontend/src/utils/getAppConfig.tsx
@@ -4,6 +4,6 @@ export const getAppConfig = () => {
     isMainnet: import.meta.env.VITE_NETWORK === 'mainnet',
     proposalContractAddress:
       import.meta.env.VITE_PROPOSAL_MANAGER_ADDRESS ||
-      'mantra17p9u09rgfd2nwr52ayy0aezdc42r2xd2g5d70u00k5qyhzjqf89q08tazu2',
+      'mantra17p9u09rgfd2nwr52ayy0aezdc42r2xd2g5d70u00k5qyhzjqf89q08tazu',
   };
 };
